26d80ccaf923e6af9f117fad6838a99f41220e96
Bruno Filippone <bruno.filippone@b-fil.com>
2013-10-05 21:55:40 +0100

    Added tasks scheduler and sbt eclipse plugin

--- src/main/scala/com/bfil/board/Main.scala
+++ src/main/scala/com/bfil/board/Main.scala
@@ -1,29 +1,32 @@
 package com.bfil.board
 
-import com.bfil.board.actors.{User, StickyNote}
+import com.bfil.board.actors.{ User, StickyNote }
 import com.bfil.board.messages.Grab
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ ActorSystem, Props }
 import com.bfil.board.messages.Drop
 import scala.concurrent.duration._
 
 object Main extends App {
-  
+
   val system = ActorSystem("board")
-  
+
   val user = system.actorOf(Props[User], "user")
   val user2 = system.actorOf(Props[User], "user2")
   val stickyNote = system.actorOf(Props[StickyNote], "stickyNote")
   val stickyNote2 = system.actorOf(Props[StickyNote], "stickyNote2")
-  
-  user ! Grab(stickyNote)
-  
-  system.scheduler.scheduleOnce(0.1 seconds)(user2 ! Grab(stickyNote))(system.dispatcher)
-  
-  system.scheduler.scheduleOnce(0.2 seconds)(user ! Drop)(system.dispatcher)
-  
-  system.scheduler.scheduleOnce(0.3 seconds)(user2 ! Grab(stickyNote))(system.dispatcher)
-  
-  system.scheduler.scheduleOnce(0.4 seconds)(user ! Grab(stickyNote2))(system.dispatcher)
-  
-  system.scheduler.scheduleOnce(0.5 seconds)(system.shutdown)(system.dispatcher)
+
+  sequence(0.05,
+    () => user ! Grab(stickyNote),
+    () => user2 ! Grab(stickyNote),
+    () => user ! Drop,
+    () => user2 ! Grab(stickyNote),
+    () => user ! Grab(stickyNote2))
+
+  def sequence(delay: Double, tasks: (() => Unit)*) = {
+    val totalTime = tasks.foldLeft(0.0)((time, task) => {
+      system.scheduler.scheduleOnce(time seconds)(task())(system.dispatcher)
+      time + delay
+    })
+    system.scheduler.scheduleOnce(totalTime seconds)(system.shutdown)(system.dispatcher)
+  }
 }
