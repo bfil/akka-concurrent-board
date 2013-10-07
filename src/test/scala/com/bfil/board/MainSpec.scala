c456bc90278beae8e93750af430870e732695a4a
Bruno Filippone <bruno.filippone@ovoenergy.com>
2013-10-07 12:02:53 +0100

    Added more tests

--- src/test/scala/com/bfil/board/MainSpec.scala
+++ src/test/scala/com/bfil/board/MainSpec.scala
@@ -1,15 +1,13 @@
 package com.bfil.board
 
 import scala.concurrent.duration.DurationInt
-
 import org.scalatest.{BeforeAndAfterAll, FlatSpec}
 import org.scalatest.matchers.ShouldMatchers
-
 import com.bfil.board.actors.{StickyNote, User}
 import com.bfil.board.messages.Grab
-
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import com.bfil.board.messages.Drop
 
 class MainSpec(_system: ActorSystem)
   extends TestKit(_system)
@@ -42,6 +40,30 @@ class MainSpec(_system: ActorSystem)
     user.underlyingActor.asInstanceOf[User].grabbedItem should be(Some(stickyNote))
     stickyNote.underlyingActor.asInstanceOf[StickyNote].grabbedBy should be(Some(user))
     user2.underlyingActor.asInstanceOf[User].grabbedItem should be(None)
+  }
+  
+  "An user" should "be able to drop a stickyNote after grabbing it" in {
+    val user = TestActorRef(Props[User])
+    val stickyNote = TestActorRef(Props[StickyNote])
+    user ! Grab(stickyNote)
+    user.underlyingActor.asInstanceOf[User].grabbedItem should be(Some(stickyNote))
+    stickyNote.underlyingActor.asInstanceOf[StickyNote].grabbedBy should be(Some(user))
+    user ! Drop
+    user.underlyingActor.asInstanceOf[User].grabbedItem should be(None)
+    stickyNote.underlyingActor.asInstanceOf[StickyNote].grabbedBy should be(None)
+  }
+  
+  "An user" should "drop the current grabbed note before grabbing a new one" in {
+    val user = TestActorRef(Props[User])
+    val stickyNote = TestActorRef(Props[StickyNote])
+    val stickyNote2 = TestActorRef(Props[StickyNote])
+    user ! Grab(stickyNote)
+    user.underlyingActor.asInstanceOf[User].grabbedItem should be(Some(stickyNote))
+    stickyNote.underlyingActor.asInstanceOf[StickyNote].grabbedBy should be(Some(user))
+    user ! Grab(stickyNote2)
+    user.underlyingActor.asInstanceOf[User].grabbedItem should be(Some(stickyNote2))
+    stickyNote2.underlyingActor.asInstanceOf[StickyNote].grabbedBy should be(Some(user))
+    stickyNote.underlyingActor.asInstanceOf[StickyNote].grabbedBy should be(None)
   }
   
 }
