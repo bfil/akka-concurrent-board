b767f20006b8d005215049b28a26178791db1c02
Bruno Filippone <bruno.filippone@ovoenergy.com>
2013-10-10 17:48:17 +0100

    Enabled remoting

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.10.2"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.2.1",
+  "com.typesafe.akka" %% "akka-remote" % "2.2.1",
   "com.typesafe.akka" %% "akka-testkit" % "2.2.1",
   "org.scalatest" %% "scalatest" % "1.9.1" % "test"
 )
--- src/main/scala/com/bfil/board/Main.scala
+++ src/main/scala/com/bfil/board/Main.scala
@@ -4,12 +4,13 @@ import scala.concurrent.duration.DurationDouble
 
 import com.bfil.board.actors.{Board, Client, Note}
 import com.bfil.board.messages.{AddNote, ConnectClient, Drop, Grab}
+import com.typesafe.config.ConfigFactory
 
 import akka.actor.{ActorSystem, Props, actorRef2Scala}
 
 object Main extends App {
 
-  val system = ActorSystem("concurrent-board")
+  val system = ActorSystem("concurrent-board", ConfigFactory.load())
 
   val board = system.actorOf(Props[Board], "board")
   val client = system.actorOf(Client.props("client"), "client")
